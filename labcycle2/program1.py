97bb3ea46a787a836b44a1fb0940291affef9cd4
ShifanaShirin <161309733+ShifanaShirin@users.noreply.github.com>
2024-12-07 14:44:46 +0530

    Delete labcycle2 directory

--- labcycle2/program1.py
+++
@@ -1,3 +0,0 @@
-str=input("Enter a string:")
-new_str=str[-1]+str[1:-1]+str[0]
-print("New String : ",new_str)
--- labcycle2/program2.py
+++
@@ -1,4 +0,0 @@
-str=input("Enter a string which has reoccurence of first character:")
-firstchar=str[0]
-newstr=firstchar+str[1:].replace(firstchar,'$')
-print("New String : ",newstr)
