0105729ed52d5855e27cf7393ffad68747bfd75e
ShifanaShirin <161309733+ShifanaShirin@users.noreply.github.com>
2024-12-07 14:18:03 +0530

    Create ticket.py

---
+++ labcycle1/ticket.py
@@ -0,0 +1,9 @@
+age=int(input("Enter your age:"))
+if age<10:
+        print("Your rate for entry ticket in this fair is 7 ")
+elif 10<=age<60:
+        print("Your rate for entry ticket in this fair is 10 ")
+elif age>=60:
+        print("Your rate for entry ticket in this fair is 5 ")
+else:
+        print("Enter a valid age")
