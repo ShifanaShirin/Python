481a1207f51e33ca2b3d738e8c48b6af0aa0f47e
ShifanaShirin <161309733+ShifanaShirin@users.noreply.github.com>
2024-12-07 14:15:52 +0530

    Create numeric.py

---
+++ labcycle1/numeric.py
@@ -0,0 +1,6 @@
+int_num=10
+print(f"Integer : {int_num} , Type : {type(int_num)}")
+float=2.5
+print(f"Float : {float} , Type : {type(float)}")
+complex_num=2+3j
+print(f"Complex Number : {complex_num} , Type : {type(complex_num)}")
