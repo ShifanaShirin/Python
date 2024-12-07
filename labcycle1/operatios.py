ef44ceb9f443fcde9cbf28a59cee94c82c58a34f
ShifanaShirin <161309733+ShifanaShirin@users.noreply.github.com>
2024-12-07 14:16:21 +0530

    Create operatios.py

---
+++ labcycle1/operatios.py
@@ -0,0 +1,14 @@
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+sum=num1+num2
+difference=num1-num2
+product=num1*num2
+quotient=num1/num2
+remainder=num1%num2
+exponent=num1**num2
+print(f"Sum : {num1} + {num2} = {sum}")
+print(f"Difference : {num1} - {num2} = {difference}")
+print(f"Product : {num1} * {num2} = {product}")
+print(f"Quotient : {num1} / {num2} = {quotient}")
+print(f"Remainder : {num1} % {num2} = {remainder}")
+print(f"Exponent : {num1} ** {num2} = {exponent}")
