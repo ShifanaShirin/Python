8400d3c4236c1414c7512f7c96e064aaf657573d
ShifanaShirin <161309733+ShifanaShirin@users.noreply.github.com>
2024-12-09 14:39:34 +0530

    Delete labcycle5 directory

--- labcycle5/program3.py
+++
@@ -1,8 +0,0 @@
-import calendar
-from datetime import date,timedelta
-today=date.today()
-yesterday=today-timedelta(days=1)
-tomorrow=today+timedelta(days=1)
-print("Yesterday = ",calendar.day_name[yesterday.weekday()])
-print("Today = ",calendar.day_name[today.weekday()])
-print("Tomorrow = ",calendar.day_name[tomorrow.weekday()])
